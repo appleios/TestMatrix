4935b2435b99228691750e6acd50deaec66685eb
Aziz Latipov <vm06lau@yandex-team.ru>
2017-11-09 12:01:49 +0300

    Initial Commit

---
+++ TestMatrix/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TestMatrix
+//
+//  Created by Aziz Latipov on 09.11.17.
+//  Copyright © 2017 Aziz L. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
