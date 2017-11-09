8c444d6a246e88522d805b423872f754082f37ee
Aziz Latipov <vm06lau@yandex-team.ru>
2017-11-09 13:28:39 +0300

    v2

--- TestMatrix/main.cpp
+++ TestMatrix/main.cpp
@@ -37,49 +37,35 @@ int main(int argc, const char * argv[])
 {
 #define K 9
 #define m m9
-    for (int k1 = 0; k1 < K; k1++) {
-        for (int k2 = 0; k2 < K; k2++) {
-            int fu = m[k1][k2];
-            for (int a11 = 0; a11 < K; a11 ++) {
-                for (int a12 = 0; a12 < K; a12++) {
-                    for (int a13 = 0; a13 < K; a13++) {
+    for (int a11 = 0; a11 < K; a11 ++) {
+        for (int a12 = 0; a12 < K; a12++) {
+            for (int a13 = 0; a13 < K; a13++) {
+                int s = 0; // количество точек где f1 единственная функция такая что f1(x1,x2) == fu(x1, x2)
+                for (int k1 = 0; k1 < K; k1++) {
+                    for (int k2 = 0; k2 < K; k2++) {
                         int f1 = (a11 * k1 + a12 * k2 + a13) % K;
+                        int fu = m[k1][k2];
                         if (f1 == fu) {
-                            int n = 0;
-                            int k = 0;
+                            int f2_different_count = 0;
+                            int f2_count = 0;
                             for (int a21 = a11+1; a21 < K; a21 ++) {
                                 for (int a22 = a12+1; a22 < K; a22++) {
                                     for (int a23 = a13+1; a23 < K; a23++) {
                                         int f2 = (a21 * k1 + a22 * k2 + a23) % K;
-                                        if (f2 != f1) {
-                                            n++;
+                                        if (f2 != fu) {
+                                            f2_different_count++;
                                         }
-                                        k++;
+                                        f2_count++;
                                     }
                                 }
                             }
-                            if (k > 0 && n == 0) {
-                                printf("%d: x1 = %d, x2 = %d, fu = %d, f1(a1: %d, a2: %d, a3: %d) = %d, k = %d\n", k1*K+k2, k1, k2, fu, a11, a12, a13, f1, k);
-
-                                int s = 0, sf = 0;
-                                for (int a21 = 0; a21 < K; a21 ++) {
-                                    for (int a22 = 0; a22 < K; a22++) {
-                                        for (int a23 = 0; a23 < K; a23++) {
-                                            if (a11 != a21 || a21 != a22 || a13 != a23) {
-                                                int f2 = (a21 * k1 + a22 * k2 + a23) % K;
-                                                if (f2 == f1) {
-                                                    s++;
-                                                }
-                                                sf++;
-                                            }
-                                        }
-                                    }
-                                }
-                                printf("s = %d, sf = %d\n", s, sf);
+                            if (f2_count == f2_different_count) {
+                                s++;
                             }
                         }
                     }
                 }
+                std::cout << s << std::endl;
             }
         }
     }
