2bbc4c91853a99cb403ffce0760331f4398332f3
Aziz Latipov <vm06lau@yandex-team.ru>
2018-01-20 19:25:43 +0300

    before cleaning out

--- TestMatrix/main.cpp
+++ TestMatrix/main.cpp
@@ -17,29 +17,72 @@ static inline int X1(int k) { return k / DIM; }
 static inline int X2(int k) { return k % DIM; }
 
 
-int matrixUniversalityQuality(Matrix m, const int K);
-
-
-void copyMatrix(Matrix src, Matrix dst, const int K);
+void generateSeed(Matrix seed, const int K);
 
 int gradientSearchWithInitialMatrix(Matrix initialMatrix, const int K);
+int matrixUniversalityQuality(Matrix m, const int K);
+
+void copyMatrix(Matrix src, Matrix dst, const int K);
+void printMatrixWithQuality(int bestQuality, Matrix matrix, const int K);
+
 
 int main(int argc, const char *argv[])
 {
     const int K = DIM;
 
-    int initialMatrix[DIM][DIM] = {
-        {1, 4, 4, 4, 2, 1},
-        {2, 0, 3, 3, 1, 5},
-        {3, 1, 4, 0, 3, 2},
-        {0, 4, 3, 2, 3, 5},
-        {5, 1, 5, 1, 0, 4},
-        {4, 0, 2, 2, 4, 3},
+    int seed[DIM][DIM] = {
+        { 5, 4, 0, 4, 1, 4, },
+        { 0, 2, 5, 5, 0, 0, },
+        { 1, 5, 4, 3, 5, 0, },
+        { 0, 5, 5, 1, 3, 2, },
+        { 4, 1, 2, 1, 3, 1, },
+        { 2, 4, 5, 2, 4, 5, },
     };
 
-    int bestQuality = gradientSearchWithInitialMatrix(initialMatrix, K);
+    int bestQualityTotal = matrixUniversalityQuality(reinterpret_cast<Matrix>(seed), DIM);
+
+
+    int bestQuality = 1;
+    int quality;
+    int tryCount = 0;
+    do {
+        tryCount++;
+
+//        generateSeed(seed, K);
+        quality = gradientSearchWithInitialMatrix(seed, K);
+
+        if (quality < bestQuality) {
+            bestQuality = quality;
+            printMatrixWithQuality(bestQuality, seed, K);
+        }
+
+        printf("iteration: %d, quality: %d, bestQuality: %d\n", tryCount, quality, bestQuality);
+    } while(bestQuality > 0);
 
     return 0;
+}
+
+void printMatrixWithQuality(int bestQuality, Matrix matrix, const int K)
+{
+    printf("bestQuality: %d\n", bestQuality);
+    printf("seed: {\n");
+    for (int i = 0; i< K; i++) {
+        printf("\t{ ");
+        for (int j = 0; j < K; j++) {
+            printf("%d, ", matrix[i][j]);
+        }
+        printf("},\n");
+    }
+    printf("}\n\n");
+}
+
+void generateSeed(Matrix seed, const int K)
+{
+    for (int i = 0; i< K; i++) {
+        for (int j = 0; j < K; j++) {
+            seed[i][j] = arc4random_uniform(DIM);
+        }
+    }
 }
 
 int gradientSearchWithInitialMatrix(Matrix initialMatrix, const int K)
@@ -55,8 +98,8 @@ int gradientSearchWithInitialMatrix(Matrix initialMatrix, const int K)
         for (int j = 0; j < K; j++) {
 
             int bestValue = currentMatrix[i][j];
-            for (int s = 0; s < K1; s++) {
-                currentMatrix[i][j] = (currentMatrix[i][j] + 1) % K;
+            for (int s = -1; s < K1; s++) {
+                currentMatrix[i][j] = s;
 
                 int quality = matrixUniversalityQuality(reinterpret_cast<Matrix>(currentMatrix), DIM);
                 if (quality < bestQuality) {
@@ -69,6 +112,7 @@ int gradientSearchWithInitialMatrix(Matrix initialMatrix, const int K)
         }
     }
 
+    printMatrixWithQuality(bestQuality, currentMatrix, K);
     return bestQuality;
 }
 
@@ -137,9 +181,9 @@ int matrixUniversalityQuality(Matrix m, const int K)
 
                 f1[0] = i / K / K; f1[1] = (i / K) % K; f1[2] = i % K;
                 f2[0] = j / K / K; f2[1] = (j / K) % K; f2[2] = j % K;
-//                printf("FAILED: for functions [%d,%d] f1:{%d * x1 + %d * x2 + %d}, f2: {%d * x1 + %d * x2 + %d}\n", i, j,
-//                f1[0], f1[1], f1[2],
-//                f2[0], f2[1], f2[2]);
+                printf("FAILED: for functions [%d,%d] f1:{%d * x1 + %d * x2 + %d}, f2: {%d * x1 + %d * x2 + %d}\n", i, j,
+                f1[0], f1[1], f1[2],
+                f2[0], f2[1], f2[2]);
 
                 failedCount++;
 
